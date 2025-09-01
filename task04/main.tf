cd079ce4de20915567a19a0369ae0ef2792909a5
o.bakhtadze <o.bakhtadze@andersenlab.com>
2025-09-01 23:42:19 +0400

    FMT

--- task04/main.tf
+++ task04/main.tf
@@ -93,13 +93,13 @@ resource "azurerm_network_interface_security_group_association" "assoc" {
 
 # Linux VM
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.vm_password
-  disable_password_authentication = false   # <- allow password login
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = var.location
+  size                            = var.vm_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.vm_password
+  disable_password_authentication = false # <- allow password login
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
@@ -113,7 +113,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"      # <- Ubuntu 22.04 LTS
+    sku       = "22_04-lts" # <- Ubuntu 22.04 LTS
     version   = "latest"
   }
 
