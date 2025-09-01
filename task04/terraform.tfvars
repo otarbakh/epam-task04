fbd2db52988ac6394864ffa653080ce45f9246ce
o.bakhtadze <o.bakhtadze@andersenlab.com>
2025-09-01 21:38:17 +0400

    Update Task04: remove sensitive vm_password from tfvars, add variable descriptions, update provider version

--- task04/terraform.tfvars
+++ task04/terraform.tfvars
@@ -1,6 +1,17 @@
-location       = "eastus2"
-address_space  = "10.90.0.0/16"
-subnet_prefix  = "10.90.1.0/24"
-admin_username = "otaradmin"
-dns_name_label = "cmaz-wbdw4cma-mod4-nginx"
-vm_password    = "Otar@12345DevOps"
+location        = "eastus2"
+rg_name         = "cmaz-wbdw4cma-mod4-rg"
+vnet_name       = "cmaz-wbdw4cma-mod4-vnet"
+subnet_name     = "frontend"
+nic_name        = "cmaz-wbdw4cma-mod4-nic"
+nsg_name        = "cmaz-wbdw4cma-mod4-nsg"
+vm_name         = "cmaz-wbdw4cma-mod4-vm"
+vm_size         = "Standard_F2s_v2"
+admin_username  = "otaradmin"
+address_space   = "10.90.0.0/16"
+subnet_prefix   = "10.90.1.0/24"
+pip_name        = "cmaz-wbdw4cma-mod4-pip"
+dns_name_label  = "cmaz-wbdw4cma-mod4-nginx"
+tags = {
+  Creator = "otar_bakhtadze@epam.com"
+}
+nsg_allow_ports = ["AllowHTTP","AllowSSH"]
--- task04/variables.tf
+++ task04/variables.tf
@@ -1,76 +1,89 @@
 variable "location" {
-  description = "Azure region"
+  description = "Azure region where resources will be deployed"
   type        = string
 }
 
 variable "rg_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-rg"
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-rg"
 }
 
 variable "vnet_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-vnet"
+  description = "Name of the Virtual Network (VNet)"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-vnet"
 }
 
 variable "subnet_name" {
-  type    = string
-  default = "frontend"
+  description = "Name of the subnet within the VNet"
+  type        = string
+  default     = "frontend"
 }
 
 variable "nic_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-nic"
+  description = "Name of the Network Interface Card (NIC) for the VM"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-nic"
 }
 
 variable "nsg_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-nsg"
+  description = "Name of the Network Security Group (NSG)"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-nsg"
 }
 
 variable "pip_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-pip"
+  description = "Name of the Public IP for the VM"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-pip"
 }
 
 variable "dns_name_label" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-nginx"
+  description = "DNS name label for the Public IP"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-nginx"
 }
 
 variable "vm_name" {
-  type    = string
-  default = "cmaz-wbdw4cma-mod4-vm"
+  description = "Name of the Virtual Machine"
+  type        = string
+  default     = "cmaz-wbdw4cma-mod4-vm"
 }
 
 variable "vm_size" {
-  type    = string
-  default = "Standard_F2s_v2"
+  description = "Size of the Virtual Machine"
+  type        = string
+  default     = "Standard_F2s_v2"
 }
 
 variable "admin_username" {
-  type    = string
-  default = "otaradmin"
+  description = "Administrator username for the VM"
+  type        = string
+  default     = "otaradmin"
 }
 
 variable "vm_password" {
-  description = "Admin password for the Linux VM (satisfy Azure Linux password policy)"
+  description = "Admin password for the Linux VM (must satisfy Azure Linux password policy)"
   type        = string
   sensitive   = true
 }
 
 variable "address_space" {
-  type    = string
-  default = "10.90.0.0/16"
+  description = "Address space for the Virtual Network"
+  type        = string
+  default     = "10.90.0.0/16"
 }
 
 variable "subnet_prefix" {
-  type    = string
-  default = "10.90.1.0/24"
+  description = "Address prefix for the subnet"
+  type        = string
+  default     = "10.90.1.0/24"
 }
 
 variable "tags" {
-  type = map(string)
+  description = "Tags to assign to all resources"
+  type        = map(string)
   default = {
     Creator = "otar_bakhtadze@epam.com"
   }
