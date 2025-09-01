017bde1605d686decacb976c76c2b5888f64b8e5
o.bakhtadze <o.bakhtadze@andersenlab.com>
2025-09-01 22:31:45 +0400

    made chanes for terraform plan

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,9 +1,7 @@
 output "vm_public_ip" {
-  description = "Public IP address of the virtual machine"
-  value       = azurerm_public_ip.main.ip_address
+  value = azurerm_public_ip.main.ip_address
 }
 
 output "vm_fqdn" {
-  description = "Fully Qualified Domain Name of the virtual machine"
-  value       = azurerm_public_ip.main.fqdn
+  value = azurerm_public_ip.main.fqdn
 }
